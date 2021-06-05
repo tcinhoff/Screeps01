ed55c792008060c1cf7e1a83c216f10dc5e26cf4
timedrix <52511392+timedrix@users.noreply.github.com>
2021-06-05 21:58:20 +0200

    Tower fixes now hovering over all Towers

--- towerRepair.js
+++ towerRepair.js
@@ -4,7 +4,7 @@ var towerRepair = {
     run: function () {
 
 
-        var towers = Game.spawns['Spawn1'].room.find(FIND_STRUCTURES, {
+        var towersR1 = Game.spawns['Spawn1'].room.find(FIND_STRUCTURES, {
             filter: (structure) => {
                 return (structure.structureType == STRUCTURE_TOWER);
             }
@@ -16,36 +16,38 @@ var towerRepair = {
             }
         });
 
-        towers.concat(towersR2);
-
+        var towers = towersR1.concat(towersR2);
         //Repair the most damaged thing
-        const targets = towers[0].room.find(FIND_STRUCTURES, {
-            filter: object => object.hits < object.hitsMax
-        });
-
-        targets.sort((a, b) => a.hits - b.hits);
-
-
-
-        if (targets.length > 0) {
-            towers[0].repair(targets[0]);
-        } else {
-            const creeps = towers[0].room.find(FIND_CREEPS, {
+        for(var i in towers) {
+            const targets = towers[i].room.find(FIND_STRUCTURES, {
                 filter: object => object.hits < object.hitsMax
             });
 
-            creeps.sort((a, b) => a.hits - b.hits);
-            towers[0].heal(creeps[0]);
-        }
+            targets.sort((a, b) => a.hits - b.hits);
 
-        //Extensions & Spawn
-        var extensions = towers[0].room.find(FIND_STRUCTURES, {
-            filter: (structure) => {
-                return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
-                    structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;}});
 
-        if(extensions.length > 0){
-            towers[0].heal(extensions[0]);
+            if (targets.length > 0) {
+                towers[i].repair(targets[0]);
+            } else {
+                const creeps = towers[0].room.find(FIND_CREEPS, {
+                    filter: object => object.hits < object.hitsMax
+                });
+
+                creeps.sort((a, b) => a.hits - b.hits);
+                towers[i].heal(creeps[0]);
+            }
+
+            //Extensions & Spawn
+            var extensions = towers[i].room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                }
+            });
+
+            if (extensions.length > 0) {
+                towers[i].heal(extensions[0]);
+            }
         }
     }
 };
